45899d8e3f4f89420f2c9d0cdee745b19afe3b40
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2023-12-04 22:56:45 +0200

    add drink item in JS

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ $('#createGoodsBtn').click(function(){
     if($('.GoodsName').val()){
         let newGoods = {
             name: $('.GoodsName').val(),
+            category: $('.GoodsCategory').val(),
+            count: $('.GoodsCount').val()
         }
         
          db.push(newGoods);
@@ -37,7 +39,18 @@ function init(){
     console.log(data)
     for(let el of data){
         $('#orderName').append(`<option value="${el.name}">${el.name}</option>`)
-        $('.drinkStorage').append(`<div class="drinkItem">${el.name}</div>`)
+        $('.drinkStorage').append(`
+        <div class="drinkItem">
+        <div class="drinkItem_buttons">
+            <button id="Order">Order</button>
+            <button id="Sell">Sell</button>
+        </div>
+        <img class="drinkItem_img" src="./img/sandora.png" alt="">
+        <div class="drinkItem_name">${el.name}</div>
+        <div class="drinkItem_taste">${el.category}</div>
+        <div class="drinkItem_count">count: ${el.count}L</div>
+    </div>
+    `)
     }
 }
 init();
